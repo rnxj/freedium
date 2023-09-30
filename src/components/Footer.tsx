0e36bff39ea839fdce05209bfe842bf3eae3ab9b
Reuel Nixon <reuelnixon@gmail.com>
2023-09-30 10:33:14 +0530

    fixing the UI: Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,8 @@ import { Button } from "./ui/button";
 
 const Footer = () => {
   return (
-    <footer className="fixed bottom-0 left-0 z-20 w-full border-t border-gray-200 bg-white p-2 shadow dark:border-gray-600 dark:bg-[#0c111c] md:flex md:items-center md:justify-center">
-      <span className="text-sm text-gray-500 dark:text-gray-400 sm:text-center">
+    <footer className="fixed bottom-0 left-0 z-20 flex w-full items-center justify-center border-t border-gray-200 bg-white p-2 shadow dark:border-gray-600 dark:bg-[#0c111c]">
+      <span className="text-center text-sm text-gray-500 dark:text-gray-400">
         Made with ❤️ by
         <Button asChild variant="link">
           <Link href="https://github.com/ReuelNixon">Reuel Nixon</Link>
