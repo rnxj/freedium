f3fb19a735fb1fb0b5d840e7647e7b579a930a89
Reuel Nixon <reuelnixon@gmail.com>
2023-09-30 07:02:04 +0530

    modified the response for an invalid input

--- src/app/api/fetchBlog/route.ts
+++ src/app/api/fetchBlog/route.ts
@@ -13,7 +13,40 @@ export async function POST(request: Request, response: Response) {
     }
   }
   if (!inList) {
-    return new Response("Please provide a valid blog site", { status: 404 });
+    return new Response(
+      `
+      <div class="container">
+        <h2>Invalid blog site</h2>
+        <p>Please enter a valid domain that's using Medium's service (include https://)</p>
+      </div>
+
+      <style>
+        .container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 80vh;
+            flex-direction: column;
+            gap: 2em;
+            max-width: 600px;
+        }
+        h2 {
+            font-size: 2em;
+            font-weight: bold;
+        }
+        p {
+            font-size: 1.5em;
+            font-weight: 500;
+            text-align: center;
+        }
+      </style>
+      `,
+      {
+        headers: {
+          "content-type": "text/html;charset=UTF-8",
+        },
+      },
+    );
   }
   const url = `http://webcache.googleusercontent.com/search?q=cache:${blogUrl}`;
 
